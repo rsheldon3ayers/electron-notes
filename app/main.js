9cf951b02734a59835dfd3b3d35f5ca5c3d70554
Robert Ayers <rsheldon3ayers@gmail.com>
2020-06-13 22:31:01 -0400

    implemented New File/Open File button function

--- app/main.js
+++ app/main.js
@@ -1,4 +1,7 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const fs = require('fs');
+
+const windows = new Set();
 
 let mainWindow = null;
 
@@ -6,15 +9,59 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow({
         show: false,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            enableRemoteModule: true
         }
     });
-    mainWindow.webContents.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
+
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
     });
-    
+
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
+    createWindow();
 })
+
+const createWindow = exports.createWindow = () => {
+    let newWindow = new BrowserWindow({ 
+        show: false,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    })
+    newWindow.loadURL(`file://${__dirname}/index.html`);
+    newWindow.once('ready-to-show', () => {
+        newWindow.show();
+    })
+
+    newWindow.on('closed', () => {
+        windows.delete(newWindow);
+        newWindow = null;
+    });
+    windows.add(newWindow);
+    return newWindow;
+}
+const getFileFromUser = exports.getFileFromUser = (targetWindow) => {
+    const files = dialog.showOpenDialog(targetWindow, {
+
+        filters: [
+            { name: 'Markdown Files', extensions: ['md', 'markdown']},
+            { name: 'Text Files', extensions: ['txt']}   
+        ],
+        properties: ['openFile']
+    }).then(result => {
+        if(files) { openFile(targetWindow, result.filePaths[0]) }
+    }).catch(err => {
+        console.log("ERROR MESSAGE:", err);
+    });
+
+    const openFile = exports.openFile = (targetWindow, file) => {
+        const content = fs.readFileSync(file).toString();
+        targetWindow.webContents.send('file-opened', file, content);
+    }    
+}
+
--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,8 @@
 const marked = require('marked');
 const sanitizeHtml = require('sanitize-html');
+const { remote, ipcRenderer } = require('electron');
+const mainProcess = remote.require('./main.js');
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -17,4 +20,16 @@ const renderMarkdownToHtml = (markdown) => {
 markdownView.addEventListener('keyup', (event) => {
     const currentContent = event.target.value;
     renderMarkdownToHtml(currentContent);
+});
+openFileButton.addEventListener('click', () => {
+    mainProcess.getFileFromUser(currentWindow);
+});
+
+newFileButton.addEventListener('click', () => {
+    mainProcess.createWindow();
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+    markdownView.value = content;
+    renderMarkdownToHtml(content);
 })
